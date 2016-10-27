ad4b69cd032fb78822dd5bcb6f70383c6e96ff73
lzepeda <lzepeda@math.mit.edu>
2016-10-27 14:34:36 -0700

    migrated the truncated functions to another file

---
+++ src/Functions.jl
@@ -0,0 +1,21 @@
+# Helper functions 
+
+@inline function solRefHelmholtz(x::Array{Float64,1},y::Array{Float64,1},z::Array{Float64,1}, sigma::Float64, k::Float64)
+  r  = sqrt( x.^2 + y.^2 + z.^2)
+  u  = (exp(-sigma^2*k^2/2)./(4*pi.*r)).*(real( exp(-1im.*k.*r).*erf( (2*sigma^2*1im*k - 2.*r)./(2*sqrt(2*sigma^2) ) )) - 1im*sin(k.*r))
+  return u
+end
+
+
+@inline function Gtruncated2D{T<:Number,N}(L::Float64,k::Float64,s::Array{T,N})
+    return (1 + (1im*pi/2*L*hankelh1(0,L*k))*(s.*besselj(1,L*s)) - (1im*pi/2*L*k*hankelh1(1,L*k))*besselj(0,L*s)  )./(s.^2 - k^2)
+end
+
+
+@inline function Gtruncated3D(L::Float64,k::Float64,s::Array{Float64,1})
+    return (-1 + exp(1im*L*k).*( cos(L*s) - (1im*k*L*sinc(L*s)) ))./( (k^2-s.^2))
+end
+
+@inline function Gtruncated3D(L::Float64,k::Float64,s::Float64)
+    return (-1 + exp(1im*L*k)*( cos(L*s) - (1im*k*L*sinc(L*s)) ))/( (k^2-s^2))
+end
